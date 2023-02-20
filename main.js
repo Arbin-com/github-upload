5691b3905122bf68670bcfc70201480204fedd26
arbin-ruisen <ruisen_pan@arbin.com>
2023-02-20 09:20:41 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,188 @@
+let a = 'java'
+let b = `hello ${a}script`
+
+const fs = require('fs');
+const { exec } = require("child_process");
+const { Octokit, App } = require("octokit");
+const { stringify } = require('querystring');
+const path = require('path');
+
+
+const execCommand = (cmd) => {
+    return new Promise((resolve, reject) => {
+        exec(cmd, (error, stdout, stderr) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+            resolve(stdout)
+        })
+    }).then(res => {
+        console.log("exec cmd:", res);
+    }).catch(err => {
+        console.log("exec cmd catch: ", err);
+    });
+}
+
+const args = process.argv;
+
+let cmdOffset = 2;
+
+const srcBranch = args[0 + cmdOffset];
+const userAndRepo = args[1 + cmdOffset];
+const token = args[2 + cmdOffset];
+const existTagName = args[3 + cmdOffset];
+const tagMessage = args[4 + cmdOffset];
+const assetses = args[5 + cmdOffset];
+const assetsePath = args[6 + cmdOffset].trim();
+
+
+const octokit = new Octokit({
+    auth: token
+})
+
+
+const isUploadVersion = srcBranch.startsWith("refs/tags/".toLocaleLowerCase());
+const isMaster = srcBranch.startsWith("refs/heads/master".toLocaleLowerCase());
+const isDev = srcBranch.startsWith("refs/heads/dev".toLocaleLowerCase());
+const isUAT = srcBranch.startsWith("refs/heads/uat".toLocaleLowerCase());
+
+if (!isUploadVersion && (isMaster || isDev || isUAT)) {
+    return;
+}
+
+
+Promise.all([execCommand(`git tag -f ${existTagName} -m ${tagMessage}`).then(() => {
+    execCommand(`git push --force origin :refs/tags/${existTagName}`).then(() => {
+        console.log("update git tag end.");
+    })
+})])
+
+if (isUploadVersion) {
+    userAndRepo = userAndRepo + "-ver";
+}
+
+let mainTask = (async (_userAndRepo, _existTagName, _tagMessage, _assetses) => {
+    arrUserAndRepo = _userAndRepo.split('/');
+    let reposPrefix = `/repos/${_userAndRepo}/releases`;
+    let getTagResult = await octokit.request('GET ' + reposPrefix + '/tags/{tag}', {
+        tag: _existTagName
+    }).catch((reason) => {
+        console.log("check release failed:")
+        console.log(reason)
+        console.log("\n\n")
+    })
+
+    if (getTagResult !== undefined) {
+        console.log("get tag result:")
+        console.log(getTagResult)
+        console.log("\n\n")
+        getTagResult = getTagResult.data
+    }
+
+    let nowDate = new Date(Date.now()).toUTCString();
+    let newReleaseData = {
+        tag_name: _existTagName,
+        name: _existTagName,
+        body: `${nowDate}\n` + _tagMessage,
+        draft: false,
+        prerelease: false
+    }
+
+
+    let newReleaseResult;
+    if (getTagResult !== undefined && getTagResult.id !== undefined) {
+        // let deleteTagResult = await octokit.request('DELETE ' + reposPrefix + '/{release_id}', {
+        //     release_id: getTagResult.id
+        // })
+        // console.log("remove old tag result:")
+        // console.log(deleteTagResult)
+        // console.log("\n\n")
+
+        let arrAssets = getTagResult.assets;
+        let arrAssetsLen = arrAssets.length
+        for (let i = 0; i < arrAssetsLen; i++) {
+            await octokit.rest.repos.deleteReleaseAsset({
+                owner: arrUserAndRepo[0],
+                repo: arrUserAndRepo[1],
+                asset_id: arrAssets[i].id,
+            }).catch((reason) => {
+                console.log("delete old assets failed:")
+                console.log(reason)
+                console.log("\n\n")
+            })
+        }
+
+        newReleaseResult = await octokit.request('PATCH ' + reposPrefix + '/{release_id}', Object.assign(newReleaseData, {
+            release_id: getTagResult.id
+        }))
+    }
+    else {
+        newReleaseResult = await octokit.request('POST ' + reposPrefix,
+            Object.assign(newReleaseData, {
+                generate_release_notes: false
+            })).catch((reason) => {
+                console.log("new release failed:")
+                console.log(reason)
+                console.log("\n\n")
+            })
+    }
+
+
+    console.log("new release:")
+    console.log(newReleaseResult)
+    console.log("\n\n")
+
+    newReleaseResult = newReleaseResult.data
+
+    let arrAssets = _assetses.split(' ');
+    if (Boolean(assetsePath)) {
+        path.join(assetsePath, dataPath)
+    }
+
+    const payloadUploadReleaseAsset = (id, dataName) => {
+        let dataPath = dataName
+        if (Boolean(assetsePath)) {
+            dataPath = path.join(assetsePath, dataName)
+        }
+        return {
+            owner: arrUserAndRepo[0],
+            repo: arrUserAndRepo[1],
+            release_id: id,
+            data: fs.readFileSync(dataPath),
+            name: dataName,
+        }
+    };
+
+
+
+    let arrAssetsLen = arrAssets.length
+    for (let i = 0; i < arrAssetsLen; i++) {
+        let assetsName = arrAssets[i];
+        // let uploadAssets = await octokit.request('POST ' + reposPrefix + '{release_id}/assets{?name}', {
+        //     release_id: newReleaseResult.id,
+        //     data: '@' + assetsName,
+        //     name: assetsName
+        // }).catch((reason) => {
+        //     console.log(`upload assets ${assetsName} failed:`)
+        //     console.log(reason)
+        //     console.log("\n\n")
+        // })
+
+        const uploadAsset = await octokit.rest.repos.uploadReleaseAsset(
+            payloadUploadReleaseAsset(newReleaseResult.id, assetsName),
+        ).catch((reason) => {
+            console.log(`upload assets ${assetsName} failed:`)
+            console.log(reason)
+            console.log("\n\n")
+        })
+
+        console.log(`upload assets ${assetsName}:\n`)
+        console.log(uploadAssets)
+        console.log("\n\n")
+    }
+
+
+})(userAndRepo, existTagName, tagMessage, assetses);
+
+Promise.all([mainTask])
---
+++ main.ps1
@@ -0,0 +1,50 @@
+
+
+function github-upload {
+    param (
+        [string] $srcBranch,
+        [string] $userAndRepo,
+        [string] $token,
+        [string] $existTagName,
+        [string] $tagMessage,
+        [string[]] $assetses
+    )
+    //ensure root path.
+    $oldPath = resolve-path ./
+    $ignoreCase = 'CurrentCultureIgnoreCase'
+    $isUploadVersion = $srcBranch.StartsWith('refs/tags/', $ignoreCase)
+    $isMaster = $srcBranch.StartsWith('refs/heads/master', $ignoreCase)
+    $isDev = $srcBranch.StartsWith('refs/heads/dev', $ignoreCase)
+    $isUAT = $srcBranch.StartsWith('refs/heads/uat', $ignoreCase)
+
+    if(!($isUploadVersion) -and ($isMaster -or $isDev -or $isUAT))
+    {
+        echo "No resources need to be uploaded"
+        return
+    }
+
+    if($isUploadVersion)
+    {
+        $userAndRepo = $userAndRepo + "-version"
+    }
+    else   
+    {
+        $userAndRepo = $userAndRepo + "-test"
+    }
+    
+    git clone https://$token@github.com/$userAndRepo
+
+    cd ./github-upload
+    npm update
+
+    cd ../
+    cd ./$userAndRepo
+
+    git config --global user.email "test@arbin.com"
+    git config --global user.name "arbin-test"
+
+    git tag -f $existTagName -m $tagMessage
+    git push --force origin :refs/tags/$existTagName
+
+    node ../github-upload/main.js $srcBranch $userAndRepo $token $existTagName $tagMessage $assetses "$oldPath"
+}
